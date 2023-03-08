bb9d6bf2d5f11527a7e7d81f0f2142c19dab61cb
N-eeraj <neeraj7rajeena@gmail.com>
2023-03-09 00:17:58 +0530

    Refactor and added check for duplicate email and token

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -1,5 +1,6 @@
 # imports
 from fastapi import HTTPException
+from sqlalchemy.orm.exc import NoResultFound
 from ..db import session
 import bcrypt
 import secrets
@@ -8,64 +9,37 @@ import secrets
 from ..models.users import User
 from ..models.tokens import Token
 
-def login_handler(request):
-    # validate user using users table
-    id = validate_login(request.email, request.password)
-
-    # get bearer token
-    token = get_bearer_token(id)
-
-    # return success message
-    return {
-        'success': True,
-        'message': 'Logged in successfully',
-        'token': token
-    }
-
 def validate_login(email, password):
     # fetch user details by email
-    results = session.query(User).filter(User.email == email).all()
-    if len(results) == 0:
-        raise HTTPException(status_code=401, detail={
+    try:
+        result = session.query(User).filter(User.email==email).one()
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail={
             'success':False,
             'message': 'User not found'
         })
 
     # check request password
-    incorrect_password = not bcrypt.checkpw(bytes(password, 'utf-8'), results[0].password.encode('utf-8'))
+    incorrect_password = not bcrypt.checkpw(bytes(password, 'utf-8'), result.password.encode('utf-8'))
     if incorrect_password:
         raise HTTPException(status_code=401, detail={
             'success':False,
             'message': 'Incorrect password'
         })
-    return results[0].id
+    return result.id
 
-def register_handler(request):
-    # check if email already exist in users table
-
-
-    # encrypt password and save to users table
-    hashed_password = bcrypt.hashpw(bytes(request.password, 'utf-8'), bcrypt.gensalt())
-    user = User(request.email, hashed_password)
+def create_user(email, password):
+    hashed_password = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+    user = User(email, hashed_password)
     session.add(user)
     session.commit()
-
-    results = session.query(User).filter(User.email == request.email).all()
-    id = results[0].id
-
-    # get bearer token
-    token = get_bearer_token(id)
-
-    # return response
-    return {
-        'success': True,
-        'message': 'Registration completed successfully',
-        'token': token
-    }
 
 def get_bearer_token(id):
     # generate bearer token
     jwt_token = secrets.token_hex(32)
+
+    if check_token_exist(jwt_token):
+        return get_bearer_token(id)
 
     # save token to tokens table
     token = Token(id, jwt_token)
@@ -74,3 +48,22 @@ def get_bearer_token(id):
 
     # return generated token
     return jwt_token
+
+def check_email_exist(email):
+    try:
+        results = session.query(User).filter(User.email==email).all()
+        if len(results):
+            raise HTTPException(status_code=409, detail= {
+                'success': False,
+                'message': 'Email already registered'
+            })
+    except NoResultFound:
+        pass
+
+def check_token_exist(token):
+    try:
+        results = session.query(Token).filter(Token.token==token).all()
+        if len(results):
+            return True
+    except NoResultFound:
+        False
--- app/routers/user.py
+++ app/routers/user.py
@@ -10,10 +10,36 @@ router = APIRouter()
 
 # login api
 @router.post('/login', tags=['Users'])
-async def test(request: userSchemas.Login):
-    return userControllers.login_handler(request)
+async def login(request: userSchemas.Login):
+    # validate user using users table
+    id = userControllers.validate_login(request.email, request.password)
+
+    # get bearer token
+    token = userControllers.get_bearer_token(id)
+
+    # return success message
+    return {
+        'success': True,
+        'message': 'Logged in successfully',
+        'token': token
+    }
 
 # register api
 @router.post('/register', tags=['Users'])
-async def test(request: userSchemas.Register):
-    return userControllers.register_handler(request)
+async def register(request: userSchemas.Register):
+    # check if email already exist in users table
+    userControllers.check_email_exist(request.email)
+
+    # encrypt password and save to users table
+    userControllers.create_user(request.email, request.password)
+
+    # get bearer token
+    id = userControllers.validate_login(request.email, request.password)
+    token = userControllers.get_bearer_token(id)
+
+    # return response
+    return {
+        'success': True,
+        'message': 'Registration completed successfully',
+        'token': token
+    }
