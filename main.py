0008f33ecd5c5a60b5d13884c7ab533cdd88d291
N-eeraj <neeraj7rajeena@gmail.com>
2023-03-15 09:50:29 +0530

    Set host from .env

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # import and run asgi server
 import uvicorn
 
+# load .env
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+host = os.getenv('HOST')
+
 if __name__ == '__main__':
-    uvicorn.run('app.api:app', reload=True)
+    uvicorn.run('app.api:app', host=host, reload=True)
